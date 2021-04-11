ef46ba8fd1b36e2d358c1ac12976d099c2170758
MOBSkuchen <71943003+MOBSkuchen@users.noreply.github.com>
2021-04-11 22:07:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,40 @@
-from youtubesearchpython import Search # install with 'pip3 install youtube-search-python'
-import string,random,os # String + Random for creating the Random chars, os for renaming the file
-from pytube import YouTube # Pytube for Downloading
-def byl(length): # Generating a Random String
+from youtubesearchpython import Search  # install with 'pip3 install youtube-search-python'
+import string, random, os  # String + Random for creating the Random chars, os for renaming the file
+from pytube import YouTube  # Pytube for Downloading
+
+def byl(length):  # Generating a Random String
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(length))
+
 def get_video_url(search):
-    allSearch = (Search(search, limit=0)) # Search for Videos with 'search' accept 0 (1)
-    giek = str(allSearch.result()) # Get the result as Json
-    r = byl(20) # Generate Random String with length 20
-    e = "'}, 'link': '" # Define what to get
-    f = "', 'shelfTitle': "
-    giek = giek.replace(e, r)
-    trash, p = giek.split(r)
-    link, trash = p.split(f) # Get the String
-    return link # Return the String
+    allSearch = (Search(search, limit=0))  # Search for Videos with 'search' accept 0 (1)
+    turn = str(allSearch.result())  # Get the result as Json
+    if not turn == "{'result': []}":
+        r = byl(20)  # Generate Random String with length 20
+        e = "'}, 'link': '"  # Define what to get
+        f = "', 'shelfTitle': "
+        turn = turn.replace(e, r)
+        trash, p = turn.split(r)
+        link, trash = p.split(f)  # Get the String
+        return link  # Return the String
+    else:
+        print("Video not found")
+        return "Error"
+
 def download_audio(url):
-    yt = YouTube(url) # Define Video
-    ys = yt.streams.get_audio_only() # Get Audio Only
-    ys.download() # Download Audio Only
-    new_title = str(str(yt.title) + ".mp3") # Get new name
-    old_title = str(str(yt.title) + ".mp4") # Get old name
-    os.rename(old_title, new_title) # Rename file
-download_audio(get_video_url("Crab Rave")) # Do Everything
+    if url == "Error":
+        return
+    yt = YouTube(url)  # Define Video
+    ys = yt.streams.get_audio_only()  # Get Audio Only
+    try:
+        ys.download()  # Download Audio Only
+        new_title = str(str(yt.title) + ".mp3")  # Get new name
+        old_title = str(str(yt.title) + ".mp4")  # Get old name
+        os.rename(old_title, new_title)  # Rename file
+    except FileExistsError:
+        return
+    except:
+        print("Could not Rename File")
+        return
+
+download_audio(get_video_url(""))  # Do Everything
